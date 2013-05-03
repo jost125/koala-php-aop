6e52a58f787e4d19ef31e9219231462cd7378a60
Jan Machala <jan.machala@email.cz>
2013-05-03 21:30:39 +0200

    Pointcut examples in comment

--- src/library/AOP/Pointcut/PointcutExpression.php
+++ src/library/AOP/Pointcut/PointcutExpression.php
@@ -2,6 +2,16 @@
 
 namespace AOP\Pointcut;
 
+/**
+ * Usage:
+ * @\AOP\Pointcut("execution([modifier] \My\Package\MyClass::myMethod(ArgumentType, ArgumentType2))")
+ *
+ * Examples:
+ * @\AOP\Pointcut("execution(public *\MyClass::*(..))")	Any method of MyClass in any package with any arguments
+ * @\AOP\Pointcut("execution(public \My\Package\*::*(..))")	Any method of any class in package \My\Package with any arguments
+ * @\AOP\Pointcut("execution(public \My\*\*::*(..))")	Any method of any class in any subpackage in package \My with any arguments
+ * @\AOP\Pointcut("execution(public \My\Package\MyClass::myMethod(var, var))")	a method \My\Package\MyClass::myMethod with unspecified two arguments
+ */
 class PointcutExpression {
 
 	private $expression;
