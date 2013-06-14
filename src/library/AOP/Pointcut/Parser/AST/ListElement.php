2bac9efcb57effbe0093f31df58f6083f65eb2fa
Jan Machala <jan.machala@email.cz>
2013-06-14 15:44:35 +0200

    Value is correctly passed

--- src/library/AOP/Pointcut/Parser/AST/ListElement.php
+++ src/library/AOP/Pointcut/Parser/AST/ListElement.php
@@ -9,12 +9,16 @@ abstract class ListElement extends Element {
 	private $value;
 
 	public function __construct($value) {
-		$this->value;
+		$this->value = $value;
 	}
 
 	public function acceptVisitor(ElementVisitor $visitor) {
 		$reflectionClass = new ReflectionClass($this);
 		$method = 'accept' . ucfirst($reflectionClass->getShortName());
 		$visitor->{$method}($this);
+	}
+
+	public function getValue() {
+		return $this->value;
 	}
 }
