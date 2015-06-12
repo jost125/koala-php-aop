ee27c3f5f9b89ff76071404415200613f10b6012
Jan Machala <jan.machala@email.cz>
2015-06-12 18:34:25 +0200

    Delete class loader dumping

--- src/Koala/loader.php
+++ src/Koala/loader.php
@@ -1,7 +1,5 @@
 <?php
 
-include_once __DIR__ . '/classloader.php';
-
 new \Koala\AOP\Aspect([]); // Hack to import
 new \Koala\AOP\Around([]); // Hack to import
 new \Koala\AOP\Before([]); // Hack to import
